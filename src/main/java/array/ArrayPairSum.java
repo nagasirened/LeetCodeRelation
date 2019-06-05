4957fc461527ab64b3c919d81af3c451b67d8c54
TS.CTU.zengguangfu <zengguangfu@travelsky.com>
2019-06-05 17:01:31 +0800

    2019-06-04 21:23:00

--- src/main/java/array/ArrayPairSum.java
+++ src/main/java/array/ArrayPairSum.java
@@ -7,6 +7,8 @@ package array;
  * @create 2019-06-03 16:05
  */
 
+import java.util.Arrays;
+
 /**
  * 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，
  * 使得从1 到 n 的 min(ai, bi) 总和最大。
@@ -20,4 +22,23 @@ package array;
      数组中的元素范围在 [-10000, 10000].
  */
 public class ArrayPairSum {
+    public static void main(String[] args) {
+        int[] nums = {1,4,3,2};
+        int result = arrayPairSum(nums);
+        System.out.println(result);
+    }
+
+    /**
+     * 思路：将数组按照从小到大排序，步进2计算总和即可
+     * 执行用时 : 37 ms, 在Array Partition I的Java提交中击败了50.99% 的用户
+       内存消耗 : 48.8 MB, 在Array Partition I的Java提交中击败了64.76% 的用户
+     */
+    public static int arrayPairSum(int[] nums) {
+        int sum = 0;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length ; i=i+2){
+            sum +=  nums[i];
+        }
+        return sum;
+    }
 }
---
+++ src/main/java/array/MatrixReshape.java
@@ -0,0 +1,97 @@
+package array;
+
+/**
+ * @program: LeetCode Study
+ * @description: 重塑矩阵
+ * @author: ZengGuangfu
+ * @create 2019-06-05 15:40
+ */
+
+/**
+ * 在MATLAB中，有一个非常有用的函数 reshape，它可以将一个矩阵重塑为另一个大小不同的新矩阵，但保留其原始数据。
+   给出一个由二维数组表示的矩阵，以及两个正整数r和c，分别表示想要的重构的矩阵的行数和列数。
+   重构后的矩阵需要将原始矩阵的所有元素以相同的行遍历顺序填充。
+   如果具有给定参数的reshape操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。
+ */
+
+/**
+ * 示例 1:
+ 输入:
+ nums =[[1,2],[3,4]]  r = 1, c = 4
+ 输出: [[1,2,3,4]]
+ 解释: 行遍历nums的结果是 [1,2,3,4]。新的矩阵是 1 * 4 矩阵, 用之前的元素值一行一行填充新矩阵。
+
+ 示例 2:
+ 输入:
+ nums =[[1,2],[3,4]]    r = 2, c = 4
+ 输出:
+ [[1,2],[3,4]]
+ 解释: 没有办法将 2 * 2 矩阵转化为 2 * 4 矩阵。 所以输出原矩阵。
+ */
+public class MatrixReshape {
+
+    public static void main(String[] args) {
+        int[][] nums = {{1,2},{3,4}};
+        int[][] result = matrixReshape(nums, 4, 1);
+        for (int[] array : result){
+            for (int num : array){
+                System.out.print(num + "  ");
+            }
+        }
+    }
+
+    /**
+     * 执行用时 : 3 ms, 在Reshape the Matrix的Java提交中击败了94.97% 的用户
+       内存消耗 : 48.9 MB, 在Reshape the Matrix的Java提交中击败了68.55% 的用户
+
+       我的思路: 首先是要判断是否能够转换，即计算元素总数，不匹配则直接返回原来的内容
+                然后元素还是双重for循环去遍历
+                遍历出来的数据填充到新的二维数组中，用两个数字辅助代表二维数组的第一个下表和第二个下标
+     */
+    public static int[][] matrixReshape(int[][] nums, int r, int c) {
+        if (nums.length * nums[0].length  != r*c){
+            System.out.println("wrong");
+            return nums;
+        }
+        int[][] result = new int[r][c];
+        int k = 0,q = 0;
+        for (int i=0; i< nums.length ; i++){
+            for (int j=0;j< nums[0].length; j++){
+                if (k < c){
+                    result[q][k] = nums[i][j];
+                    k++;
+                }else{
+                    k = 0; q++;
+
+                    result[q][k] = nums[i][j];
+                    k++;
+                }
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * 这是其他人写的东西
+     * TODO 利用除法的商值和取余的值，逻辑上需要有一定的理解，值得学习
+     */
+    public static int[][] matrixReshape2(int[][] nums, int r, int c) {
+        int row = nums.length;
+        if (row == 0) {
+            return nums;
+        }
+        int col = nums[0].length;
+        if (row * col != r * c) {
+            return nums;
+        }
+
+        int total = r * c;
+        int[][] res = new int[r][c];
+        for (int t = 0; t < total; t++) {
+            res[t / c][t % c] = nums[t / col][t % col];
+        }
+
+        return res;
+    }
+}
