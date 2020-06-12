a11ec091a3aad1b2f17bab887a482633c3bf8094
KatoUyi <32826195+nagasirened@users.noreply.github.com>
2020-06-12 18:00:31 +0800

    20200612

--- src/main/java/digit/m/AddTwoNumbers.java
+++ src/main/java/digit/m/AddTwoNumbers.java
@@ -1,11 +1,5 @@
 package digit.m;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-
-import java.util.Stack;
-
 /**
  * author: ZGF
  * 06-2020/6/11 : 10:02
@@ -32,33 +26,36 @@ import java.util.Stack;
 
 public class AddTwoNumbers {
 
-    public static void main(String[] args) {
-
-    }
-
+    /**
+     * 按照 个十百千万的顺序累计叠加，如果没有则补0
+     */
     public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
-        ListNode pre = new ListNode(0);        // head节点，最后会去掉
-        ListNode cur = pre;
-        int carry = 0;
-        while(l1 != null || l2 != null) {
+        ListNode prev = new ListNode(0);
+        ListNode cur = prev;
+        int current = 0;
+        while (l1 != null || l2 != null){
             int x = l1 == null ? 0 : l1.val;
             int y = l2 == null ? 0 : l2.val;
-            int sum = x + y + carry;
+            int sum = x + y + current;
 
-            carry = sum / 10;                    // carry 代表了进1
+            // 进数
+            current = sum / 10;
+            // 余数
             sum = sum % 10;
             cur.next = new ListNode(sum);
-
             cur = cur.next;
-            if(l1 != null)
+
+            if (l1 != null){
                 l1 = l1.next;
-            if(l2 != null)
+            }
+            if (l2 != null){
                 l2 = l2.next;
+            }
         }
-        if(carry == 1) {
-            cur.next = new ListNode(carry);
+        if (current > 0){
+            cur.next = new ListNode(current);
         }
-        return pre.next;
+        return prev.next;
     }
 
 }
---
+++ src/main/java/string/m/LengthOfLongestSubstring.java
@@ -0,0 +1,66 @@
+package string.m;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashSet;
+
+/**
+ * author: ZGF
+ * 06-2020/6/12 : 15:20
+ * context : 无重复字符的最长子串
+ */
+
+/*
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+
+输入: "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+*/
+
+public class LengthOfLongestSubstring {
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        int i = lengthOfLongestSubstring(s);
+        System.out.println(i);
+    }
+
+    /*
+    思路：
+        遍历字符串，每次以 i 值记录，不回溯 i 值，用flag记录遍历过程找到的重复的字符的位置。
+        如果遇到重复字符，i-flag 即为子串长度，此时flag重新定位到子串中重复字符的位置，i 继续往后遍历。
+        这里length跟result记录长度。我感觉代码可以更简洁一点的，但是好像写懵了？
+     */
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0){
+            return 0;
+        }
+        int max = 0, i = 0, j = 1;
+        HashSet<Character> set = new HashSet<>();
+        set.add(s.charAt(i));
+        while (j < s.length()){
+            if (set.contains(s.charAt(j))){
+                set.remove(s.charAt(i++));
+            }else {
+                set.add(s.charAt(j++));
+            }
+            max = Math.max(max, j - i);
+        }
+        return max;
+    }
+}
