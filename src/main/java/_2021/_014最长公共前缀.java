fb313f0c358a5d24e70ef4713bbbac80d00892ba
guangfuz <guangfuz@rings.net.cn>
2021-09-23 19:53:31 +0800

    014题提交

---
+++ src/main/java/_2021/_014最长公共前缀.java
@@ -0,0 +1,31 @@
+package _2021;
+
+/**
+ * 示例 1：
+ *
+ * 输入：strs = ["flower","flow","flight"]
+ * 输出："fl"
+ * 示例 2：
+ *
+ * 输入：strs = ["dog","racecar","car"]
+ * 输出：""
+ * 解释：输入不存在公共前缀。
+ */
+public class _014最长公共前缀 {
+
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        String str = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            String item = strs[i];
+            while (item.indexOf(str) != 0 && str.length() != 0) {
+                str = str.substring(0, str.length() - 1);
+            }
+        }
+        return str;
+    }
+}
