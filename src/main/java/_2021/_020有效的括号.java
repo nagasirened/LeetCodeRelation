d38a6a14c3fd5dc6934d304b143372d9c2b1d3c7
guangfuz <guangfuz@rings.net.cn>
2021-09-24 09:47:10 +0800

    020题提交

---
+++ src/main/java/_2021/_020有效的括号.java
@@ -0,0 +1,34 @@
+package _2021;
+
+import java.util.Stack;
+
+public class _020有效的括号 {
+
+    public static void main(String[] args) {
+        System.out.println(isValid("([)]"));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                if ((c == ')' && stack.pop() != '(') ||
+                        (c == '}' && stack.pop() != '{') ||
+                        (c == ']' && stack.pop() != '[') ) {
+                    return false;
+                }
+            }
+        }
+        if (!stack.empty()) {
+            return false;
+        }
+
+        return true;
+    }
+}
